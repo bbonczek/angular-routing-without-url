486e61075eb3a58efc958c1ffb91390b2f54f611
Bartosz Baczek <bbaczek@rockwellautomation.com>
2020-07-11 14:48:34 +0200

    some changes, not sure what

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,14 @@ import { Component } from '@angular/core';
     <h3 class="pure-u-1 center">This is a hosting application. It can be a shell for an application that is using it's own routing.</h3>
     <h3 class="pure-u-1 center">It would be better if 'children' applications didn't interfere with this page routing</h3>
   </div>
-  <a class="pure-button" href="/page-from-host">Open page from Host</a>
-  <a class="pure-button" href="/page-with-nested-app">Open page from Host</a>
-  <div class="pure-u-1 nested-app-space">
-      <router-outlet></router-outlet>
+  <div class="pure-g">
+    <div class="pure-u-1 main-div">
+    <a class="pure-button" href="/page-from-host">Open page from Host</a>
+    <a class="pure-button" href="/page-with-nested-app">Open page from Host</a>
+    <div class="pure-u-1-3 nested-app-space">
+        <router-outlet></router-outlet>
+    </div>
+    </div>
   </div>
   `,
   styles: [`
@@ -19,11 +23,14 @@ import { Component } from '@angular/core';
       text-align: center;
     }
 
+    .main-div {
+      background: pink;
+      justify-content: center;
+    }
+
     .nested-app-space {
       min-height: 700px;
       background: green;
-      display: flex;
-      flex-direction: column;
     }
 
     .nested-app-space *{
