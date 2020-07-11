c1f01ba4a88e1d2cc24508807ea554f73a160db9
Bartosz Baczek <bbaczek@rockwellautomation.com>
2020-07-11 16:02:29 +0200

    beautifying it a bit

--- projects/app1/src/app/app.component.ts
+++ projects/app1/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { Component } from '@angular/core';
   selector: 'nested-app-root',
   template: `
   <div class="pure-g">
-    <h1 class="pure-u-1 center">Nested App</h1>
-    <h3 class="pure-u-1 center">It is a nested app as well. It has its own routing as well - click different buttons to verify that</h3>
+    <h1 class="pure-u-1 center">Child application</h1>
+    <p class="pure-u-1 center">The test you are reading is already a nested application. It's route was defined in host application's app-routing.module, but content is another application. As you can see below, there are 2 buttons - they will change your route as well. They are a bit different than the ones in the sidenav though - they are in 100% part of the nested application. Try to change your current route using them.</p>
+    <p class="pure-u-1 center">Hopefully, your route will change, but you will not see it in the address bar in your browser.</p>
   </div>
-  <a class="pure-button pure-button-primary" routerLink="/page-with-nested-app/first-page-from-nested-app">First page from nested app</a>
-  <a class="pure-button pure-button-primary" routerLink="/page-with-nested-app/second-page-from-nested-app">Second page from nested app</a>
+  <a class="pure-button pink" routerLink="/page-with-nested-app/first-page-from-nested-app">First page from nested app</a>
+  <a class="pure-button pink" routerLink="/page-with-nested-app/second-page-from-nested-app">Second page from nested app</a>
   <div class="pure-u-1 nested-app-space">
     <router-outlet></router-outlet>
   </div>
@@ -21,6 +22,10 @@ import { Component } from '@angular/core';
     .nested-app-space {
       background: yellow;
       height: 400px;
+    }
+
+    .pink {
+      background-color: #ED1C5B;
     }
   `]
 })
--- src/app/page-from-host-app.component.ts
+++ src/app/page-from-host-app.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-host-app-page',
   template: `
   <div class="pure-u-1">
-    <h1>This is a page that resides directly in host application</h1>
+    <h1>About</h1>
+    <p>This is a page that is a part of the host application. It is using just a regular hash routing mechanism.</p>
+    <p>There isn't anything special in it.</p>
   </div>
   `,
 })
